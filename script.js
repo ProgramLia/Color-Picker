37614e8e88c6621dd4a78cb9bcae08dd7951fcd5
programlia <programlia@gmail.com>
2025-02-22 20:26:23 +0800

    tambahkan fitur copy text

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ input.addEventListener('input' , function(event) {
     hexa.textContent = input.value;
     document.body.style.backgroundColor = input.value
 })
+
+// fungsi untuk copy text...
+function copy() {
+    let text = input.value;
+    navigator.clipboard.writeText(text).then(item=> {
+        document.querySelector('.information').textContent = 'Text Berhasil disalin'
+    }).catch(error=> {
+        console.log('Gagal menyalin text' + error)
+    })
+}
